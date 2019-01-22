7810dbe71e697447af20d51668d977e5bd707b14
Jenniffer Helena Alvarez Puello - Ceiba Software <jenniffer.alvarez@ceiba.com.co>
2019-01-22 09:26:57 -0500

    First commit

---
+++ src/main/java/com/ceiba/estacionamiento/jenniffer/alvarez/CeibaEstacionamiento/jenniffer/alvarez/Application.java
@@ -0,0 +1,14 @@
+package com.ceiba.estacionamiento.jenniffer.alvarez.CeibaEstacionamiento.jenniffer.alvarez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
