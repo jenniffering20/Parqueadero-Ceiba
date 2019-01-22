890bc794ee7fd5b6edeafd90038b002767878c86
Jenniffer Helena Alvarez Puello - Ceiba Software <jenniffer.alvarez@ceiba.com.co>
2019-01-22 14:41:53 -0500

    ejemplo 3 test

--- src/test/java/com/ceiba/estacionamiento/jenniffer/alvarez/CeibaEstacionamiento/jenniffer/alvarez/ApplicationTests.java
+++ src/test/java/com/ceiba/estacionamiento/jenniffer/alvarez/CeibaEstacionamiento/jenniffer/alvarez/ApplicationTests.java
@@ -7,14 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.ceiba.estacionamiento.jenniffer.alvarez.Person;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ApplicationTests {
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	
 	
 
-}
+//}
 
